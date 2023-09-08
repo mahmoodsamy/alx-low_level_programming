bab9714cdc7c5077874184ada097e41ab31d3400
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-08 16:49:37 -0700

    this is first commit in C repo

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,9 +6,10 @@
  * Description: A C program that prints with printf function
  *
  * Return: Always 0 (Success)
- */
+*/
+
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, %d");
+	printf("with proper grammar, but the outcome is a piece of art, %n");
 	return (0);
 }
