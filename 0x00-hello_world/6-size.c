bc189c602fcd262cc5d2886a5d0ea16833d21ed4
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-08 17:33:59 -0700

    this is first commit in C repo

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,12 +7,13 @@
  *
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-	printf("Size of a char: %u 1 byte(s)\n", SizeOf(char));
-	printf("Size of an int:  %u 1 byte(s)\n", SizeOf(int));
-	printf("Size of a long int: %u 1 byte(s)\n", SizeOf(long int));
-	printf("Size of a long long int: %u 1 byte(s)\n", SizeOf(long long int));
-	printf("Size of a float: %u 1 byte(s)\n", SizeOf(float));
+	printf("Size of a char: %u byte(s)\n", SizeOf(char));
+	printf("Size of an int: %u byte(s)\n", SizeOf(int));
+	printf("Size of a long int: %u byte(s)\n", SizeOf(long int));
+	printf("Size of a long long int: %u byte(s)\n", SizeOf(long long int));
+	printf("Size of a float: %u byte(s)\n", SizeOf(float));
 	return (0);
 }
