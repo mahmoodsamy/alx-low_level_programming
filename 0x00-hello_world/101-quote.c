d592862560d7c4a874d0f1cda9077b76c2441e20
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-08 18:31:34 -0700

    this is first commit in C repo

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,7 +9,8 @@
  */
 
 int main(void)
-{    char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+{    char quote[];
+quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1, quote, sizeof(quote) - 1);
 return (1);
 }
