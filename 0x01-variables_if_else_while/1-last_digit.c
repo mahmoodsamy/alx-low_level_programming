e84bbbf030851b2fdfdb9086cc9dfadd3eb1d4e5
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-10 15:41:22 -0700

    this is first commit in C repo

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,20 +8,20 @@
  * Description: This program generates last gigit of a random number and
  *              checks if it's positive, zero, or negative.
  *
- * Return: Always 0 (Success)*/
+ * Return: Always 0 (Success)
+*/
 
 int main(void)
 {
 	int n, digit;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	digit = n % 10;
 	if(digit < 6 && digit != 0)
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit );
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, digit);
 	else if(digit > 5)
-		printf("Last digit of %d is %d and is greater than 5\n", n, digit );
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	else if(digit == 0)
-		printf("Last digit of %d is %d and is 0\n", n, digit );
+		printf("Last digit of %d is %d and is 0\n", n, digit);
 	return(0);
 }
