6e8f7b8d19a346414fc87bac256701260fd590f5
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-10 12:45:51 -0700

    this is first commit in C repo

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
 int main(void)
 {
     srand(time(0));
-    int n = rand() % 2000000001 - 1000000000;
+    n = rand() % 2000000001 - 1000000000;
     if (n > 0)
     {
         printf("%d is positive \n", n);
