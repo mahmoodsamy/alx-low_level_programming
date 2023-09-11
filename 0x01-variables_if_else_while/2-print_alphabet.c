2556b793a104216d56cdda58b0e6f346945b2f63
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-10 18:12:41 -0700

    this is first commit in C repo

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - Entery point 
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+in main (void)
+{
+char letters;
+for (letters = 'a'; letters <= 'z'; letters++) {
+putchar(letters)
+}
+}
