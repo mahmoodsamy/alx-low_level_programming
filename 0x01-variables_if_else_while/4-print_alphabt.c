fdd2e15c9697e65bd9e237e52aa7f63fb783441e
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-10 19:19:31 -0700

    this is first commit in C repo

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program generates last gigit of a random number and
+ *              checks if it's positive, zero, or negative.
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char lower = 'a';
+while (lower <= 'z')
+{
+if (lower != 'e' && lower != 'q')
+{
+putchar(lower);
+}
+lower++;
+}
+return (0);
+}
