18823c9da4726b2c513654384991a480c6c4096b
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-10 19:47:14 -0700

    this is first commit in C repo

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: print all single digit numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+int num;
+for (num = 1; num > 10; num++)
+{
+prinf("%d", num);
+}
+return (0);
+}
