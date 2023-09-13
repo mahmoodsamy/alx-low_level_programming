f35e426d3c1c704bd98ac1a3d7f51112faab294f
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-12 19:26:49 -0700

    this is first commit in C repo

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program print _putchar 
+ * 
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+printf("_putchar\n");
+return (0);
+}
