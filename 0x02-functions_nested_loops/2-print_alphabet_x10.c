a93294a52991d7425dad6124c58b289e1c9c8d56
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-13 18:38:39 -0700

    this is first commit in C repo

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function that will print the alphabet 10 times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+int c, i;
+
+for (i = 1; i <= 10; i++)
+{
+for (c = 'a'; a <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+return (0);
+}
