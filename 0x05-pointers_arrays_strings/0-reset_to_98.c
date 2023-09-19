d713b7f28ba862804a06808cb6fcadba92b215f9
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-19 16:36:00 -0700

    this is first commit in C repo

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes a pointer as a prameter 
+ * anf update the value that point to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
