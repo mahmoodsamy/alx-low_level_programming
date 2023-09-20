90353f1f96f2d5f20312ef14a178c9087a1b49c0
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-19 17:12:35 -0700

    this is first commit in C repo

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 #include "main.h"
+
 /**
- * swap_int swaps the values of two integers
- * using two input parameters
- * 
+ * swap_int - swaps the values of two integers
+ * using two input parameter
  * @a: input parameter 1
  * @b: input parameter 2
  *
