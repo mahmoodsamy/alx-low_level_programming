9c812e68266f0499aff0db8c2c98c6dc92e0ea2a
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-19 18:10:02 -0700

    this is first commit in C repo

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_rev - prints reversed string, followed by new line
+ * @s: pointer to string
+ * Return: void
+*/
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int i;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = lenth - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+
+}
