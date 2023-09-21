ce2cdd90e427930ceab5835652469e319307b9cf
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-21 12:24:47 -0700

    this is first commit in C repo

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,20 +10,17 @@
  * Return: pointer to resulting strings @dest
 */
 
-char *_stract(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 	int c, c2;
 
 	c = 0;
 
 	while (dest[c])
-	{
 		c++;
-	}
 
 	for (c2 = 0; src[c2]; c2++)
-	{
 		dest[c++] = src[c2];
-	}
+
 	return (dest);
 }
