f7c412bb9e049e1af7b9f9a87b593bfc608c2c68
mahmoodsamy <mahmoudsamypk@gmail.com>
2023-09-25 19:06:10 -0700

    first commit in multi dimensional arrays project

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+some tasks with pointers and multi dimensional array
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ *
+ * Return: 1 if success
+ * -1 in error case
+*/
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
